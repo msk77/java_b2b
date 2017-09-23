51c48d567ba795c10ea118753c37deee17a77c02
Igor <roomex@gmail.com>
2017-09-23 14:44:30 -0700

    Создан пакет для первой программы в Sandbox

--- sandbox/src/main/java/ru/igorqa/sandbox/InitFirstProgram.java
+++ sandbox/src/main/java/ru/igorqa/sandbox/InitFirstProgram.java
@@ -1,7 +1,9 @@
+package ru.igorqa.sandbox;
+
 public class InitFirstProgram {
 	
 	public static void main (String[] args) {
 		
-		System.out.println("Hello, world!");
+		System.out.println("Hello1, world!");
 	}
 }
